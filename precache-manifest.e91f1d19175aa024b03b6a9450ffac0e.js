fc2948c0f9ef60767f06ad86175832c2fb18e358
Alexis <ali.finney@gmail.com>
2020-07-01 17:20:54 -0700

    Updates

--- precache-manifest.e91f1d19175aa024b03b6a9450ffac0e.js
+++ precache-manifest.e91f1d19175aa024b03b6a9450ffac0e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "35c04a77be41fe9b57ee39c2e26d78a1",
+    "revision": "bfff5e8fa44f00b39401c4a133c9a32c",
     "url": "/voteaz/index.html"
   },
   {
-    "revision": "31036278af6b6abf94b6",
+    "revision": "de1afa766cfc19bf7f55",
     "url": "/voteaz/static/css/main.f5aa22de.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/voteaz/static/js/2.f9cdcd90.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "31036278af6b6abf94b6",
-    "url": "/voteaz/static/js/main.620d3c94.chunk.js"
+    "revision": "de1afa766cfc19bf7f55",
+    "url": "/voteaz/static/js/main.51c046bc.chunk.js"
   },
   {
     "revision": "b9eea1b2e8a9481c0f50",
